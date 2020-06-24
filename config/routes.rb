fc8ffb1b0f53825f0b9bf96a6f011bc5a347e2d1
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-06-24 21:20:27 +0100

    restrict actions for unauthorized users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :users, only: %i[show updtae]
+      scope '/auth' do
+        post '/signin', to: 'user_token#create'
+        post '/signup', to: 'users#create'
+      end
     end
   end
 end
